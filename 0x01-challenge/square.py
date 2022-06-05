0e749e1adedc134bb87eec9206aa24e28edbf45a
Marcosozhol <3956@holbertonschool.com>
2022-06-05 09:44:52 -0500

    Correction error

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Defines class square"""
+
 
 class square():
-    
+    """Class square"""
     width = 0
     height = 0
 
-    
+
     def __init__(self, *args, **kwargs):
+        """init method"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +18,15 @@ class square():
         return self.height * self.width
 
     def permiter_of_my_square(self):
+        """ Perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Str representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """check if main"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
